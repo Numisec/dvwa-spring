a76129cfcc3a42da7e9cb73aec8019700586e022
if08017@gmail.com <if08017@gmail.com>
2018-06-07 10:43:07 +0700

    chore(dvwa): fist upload

--- build.groovy
+++ build.groovy
@@ -19,7 +19,7 @@ node {
 		def engagement = "/api/v1/engagements/1/"
 		def type = "arachni"
 		def scan_type = "Arachni Scan"
-		def src = "/dvwa-spring/reports/arachni-report.json"
+		def src = "/dvwa-spring/reports/${type}-report.json"
     sh "jq 'if .values[2].key == \"engagements_path\" then  .values[2].value=\"${engagement}\" | .values[3].value=\"${src}\" | .values[4].value=\"${scan_type}\"  else \"Some error\" end' postman/DefectDojo.postman_environment.json > postman/${type}.DefectDojo.postman_environment.json"
 		sh ("docker run --rm -v `pwd`:/dvwa-spring postman/newman_ubuntu1404 run /dvwa-spring/postman/DefectDojo.postman_collection.json -e /dvwa-spring/postman/${type}.DefectDojo.postman_environment.json -k")
 	}
@@ -27,7 +27,7 @@ node {
 		def engagement = "/api/v1/engagements/1/"
 		def type = "arachni"
 		def scan_type = "Arachni Scan"
-		def src = "/dvwa-spring/reports/arachni-report.json"
+		def src = "/dvwa-spring/reports/${type}-report.json"
 		sh "jq 'if .values[2].key == \"engagements_path\" then  .values[2].value=\"${engagement}\" | .values[3].value=\"${src}\" | .values[4].value=\"${scan_type}\"  else \"Some error\" end' postman/DefectDojo.postman_environment.json > postman/${type}.DefectDojo.postman_environment.json"
 		sh ("docker run --rm -v `pwd`:/dvwa-spring postman/newman_ubuntu1404 run /dvwa-spring/postman/DefectDojo.postman_collection.json -e /dvwa-spring/postman/${type}.DefectDojo.postman_environment.json -k")
 	}
@@ -35,7 +35,7 @@ node {
 		def engagement = "/api/v1/engagements/3/"
 		def type = "retirejs"
 		def scan_type = "Retire.js Scan"
-		def src = "/dvwa-spring/reports/retirejs-report.json"
+		def src = "/dvwa-spring/reports/${type}-report.json"
 		sh "jq 'if .values[2].key == \"engagements_path\" then  .values[2].value=\"${engagement}\" | .values[3].value=\"${src}\" | .values[4].value=\"${scan_type}\"  else \"Some error\" end' postman/DefectDojo.postman_environment.json > postman/${type}.DefectDojo.postman_environment.json"
 		sh ("docker run --rm -v `pwd`:/dvwa-spring postman/newman_ubuntu1404 run /dvwa-spring/postman/DefectDojo.postman_collection.json -e /dvwa-spring/postman/${type}.DefectDojo.postman_environment.json -k")
 	}
